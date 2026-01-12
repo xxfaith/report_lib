c3b6dc9ff5db713ab7cf0f3b290c85ccff53c23a
xxfaith <zhonghui@xfaith-tech.com>
2026-01-12 13:35:54 +0800

    update bundle

--- bundle.js
+++ bundle.js
@@ -1,2 +1,170 @@
-import c from"@system.fetch";import h from"@system.cipher";import f from"@system.mas.device";import p from"@system.launch";var u=["https://whois.pconline.com.cn/ipJson.jsp?json=true","https://cdid.c-ctrip.com/model-poc2/h","https://vv.video.qq.com/checktime?otype=ojson","https://g3.letv.com/r?format=1","https://r.inews.qq.com/api/ip2city","https://myip.ipip.net/json","https://i.news.qq.com/api/ip2city","https://ipv4.gdt.qq.com/get_client_ip"];async function a(e){return e>=u.length?"":c.fetch({url:u[e]}).then(n=>{let t=/\b(?:\d{1,3}\.){3}\d{1,3}\b/g,r=n.data.match(t)||[];return r.length?r[0]:a(e+1)},n=>a(e+1))}function b(e,n){return c.fetch({url:`https://api.github.com/repos/xxfaith/${n}`,header:{Accept:"application/json",Authorization:`Bearer ${e}`,"User-Agent":"repo-checker"}})}function m(e,n){return c.fetch({url:`https://api.github.com/repos/xxfaith/${n.vendor}/contents/${n.uuid}_${n.mac}.json`,responseType:"json",method:"GET",header:{Accept:"application/json",Authorization:`Bearer ${e}`,"User-Agent":"file-creator","Content-Type":"application/json",Host:"api.github.com"}}).then(t=>t.data.content?h.base64Decode(t.data.content).then(r=>{let i=new Uint8Array(r),o="";for(let s=0;s<i.length;s++)o+=String.fromCharCode(i[s]);return{content:o,sha:t.data.sha}}):{content:"",sha:t.data.sha},t=>(console.warn(t),{content:"",sha:""}))}function g(e,n,t){return h.base64Encode(JSON.stringify(t)).then(r=>c.fetch({url:`https://api.github.com/repos/xxfaith/${t.vendor}/contents/${t.uuid}_${t.mac}.json`,method:"PUT",header:{Accept:"application/json",Authorization:`Bearer ${e}`,"User-Agent":n?"file-upsert":"file-creator","Content-Type":"application/json",Host:"api.github.com"},data:{message:"update",content:r,sha:n}}))}function l(e){return c.fetch({url:"http://120.26.107.114/api/production/activate",method:"POST",responseType:"json",headers:{"Content-Type":"application/json"},data:e})}function d(e,n){return m(e,n).then(t=>a(0).then(r=>{let i={},o=JSON.parse(t.content);return console.info(o),o.createdAt?i.createdAt=o.createdAt:i.createdAt=new Date().getTime(),i.updatedAt=new Date().getTime(),g(e,t.sha,{...n,ip:r,...i})}))}function j(e){f.getInfo().then(n=>{if(n.code!==200){console.info("get device info failed"),p.exit();return}let t=JSON.parse(n.data),r=t.vendorId>>16&65535,i=t.vendorId&65535,o={vendor:r,production_id:i,mac:t.mac,uuid:t.uuid,runtime_version:t.runtimeVersion,nucleus_version:t.nucleusVersion,sdk_info:t.sdk_info};return l(o).then(s=>{if(s.data.code!==0)return d(e,o)},()=>d(e,o))}).finally(()=>{p.exit()})}export{b as checkProject,m as getFileSha,a as getIP,j as report,g as updateFile};
+// src/libs/github.ts
+import SysFetch from '@system.fetch';
+import SysCipher from '@system.cipher';
+import SysMasDevice from '@system.mas.device';
+import SysLaunch from '@system.launch';
+var host = [
+  'https://whois.pconline.com.cn/ipJson.jsp?json=true',
+  'https://cdid.c-ctrip.com/model-poc2/h',
+  'https://vv.video.qq.com/checktime?otype=ojson',
+  'https://g3.letv.com/r?format=1',
+  'https://r.inews.qq.com/api/ip2city',
+  'https://myip.ipip.net/json',
+  'https://i.news.qq.com/api/ip2city',
+  'https://ipv4.gdt.qq.com/get_client_ip',
+];
+async function getIP(index) {
+  if (index >= host.length) return '';
+  return SysFetch.fetch({
+    url: host[index],
+  }).then(
+    (res) => {
+      const ipRegex = /\b(?:\d{1,3}\.){3}\d{1,3}\b/g;
+      const ips = res.data.match(ipRegex) || [];
+      if (ips.length) return ips[0];
+      return getIP(index + 1);
+    },
+    (_) => {
+      return getIP(index + 1);
+    },
+  );
+}
+function checkProject(token, vendorId) {
+  return SysFetch.fetch({
+    url: `https://api.github.com/repos/xxfaith/${vendorId}`,
+    header: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+      'User-Agent': 'repo-checker',
+    },
+  });
+}
+function getFileSha(token, info) {
+  return SysFetch.fetch({
+    url: `https://api.github.com/repos/xxfaith/${info.vendor}/contents/${info.uuid}_${info.mac}.json`,
+    responseType: 'json',
+    method: 'GET',
+    header: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`,
+      'User-Agent': 'file-creator',
+      'Content-Type': 'application/json',
+      Host: 'api.github.com',
+    },
+  }).then(
+    (res) => {
+      if (!res.data.content)
+        return {
+          content: '',
+          sha: res.data.sha,
+        };
+      return SysCipher.base64Decode(res.data.content).then((buf) => {
+        const uint8Array = new Uint8Array(buf);
+        let decodedData = '';
+        for (let i = 0; i < uint8Array.length; i++) {
+          decodedData += String.fromCharCode(uint8Array[i]);
+        }
+        return {
+          content: decodedData,
+          sha: res.data.sha,
+        };
+      });
+    },
+    (err) => {
+      console.warn(err);
+      return {
+        content: '',
+        sha: '',
+      };
+    },
+  );
+}
+function updateFile(token, fileSha, info) {
+  return SysCipher.base64Encode(JSON.stringify(info)).then((content) => {
+    return SysFetch.fetch({
+      url: `https://api.github.com/repos/xxfaith/${info.vendor}/contents/${info.uuid}_${info.mac}.json`,
+      method: 'PUT',
+      header: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${token}`,
+        'User-Agent': fileSha ? 'file-upsert' : 'file-creator',
+        'Content-Type': 'application/json',
+        Host: 'api.github.com',
+      },
+      data: {
+        message: 'update',
+        content,
+        sha: fileSha,
+      },
+    });
+  });
+}
+function reportOfficeServer(info) {
+  return SysFetch.fetch({
+    url: 'http://120.26.107.114/api/production/activate',
+    method: 'POST',
+    responseType: 'json',
+    // @ts-ignore
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: info,
+  });
+}
+function reportGithub(token, info) {
+  return getFileSha(token, info).then((fileInfo) => {
+    return getIP(0).then((ip) => {
+      const uploadDateInfo = {};
+      const fileJson = JSON.parse(fileInfo.content);
+      console.info(fileJson);
+      if (fileJson['createdAt']) {
+        uploadDateInfo['createdAt'] = fileJson['createdAt'];
+      } else {
+        uploadDateInfo['createdAt'] = /* @__PURE__ */ new Date().getTime();
+      }
+      uploadDateInfo['updatedAt'] = /* @__PURE__ */ new Date().getTime();
+      return updateFile(token, fileInfo.sha, {
+        ...info,
+        ip,
+        ...uploadDateInfo,
+      });
+    });
+  });
+}
+function report(token) {
+  SysMasDevice.getInfo()
+    .then((res) => {
+      if (res.code !== 200) {
+        console.info('get device info failed');
+        SysLaunch.exit();
+        return;
+      }
+      const deviceInfo = JSON.parse(res.data);
+      const vendorId = (deviceInfo['vendorId'] >> 16) & 65535;
+      const productId = deviceInfo['vendorId'] & 65535;
+      const info = {
+        vendor: vendorId,
+        production_id: productId,
+        mac: deviceInfo['mac'],
+        uuid: deviceInfo['uuid'],
+        runtime_version: deviceInfo['runtimeVersion'],
+        nucleus_version: deviceInfo['nucleusVersion'],
+        sdk_info: deviceInfo['sdk_info'],
+      };
+      return reportOfficeServer(info).then(
+        (res2) => {
+          if (res2.data.code !== 0) {
+            return reportGithub(token, info);
+          }
+        },
+        () => {
+          return reportGithub(token, info);
+        },
+      );
+    })
+    .finally(() => {
+      SysLaunch.exit();
+    });
+}
+export { checkProject, getFileSha, getIP, report, updateFile };
 //# sourceMappingURL=bundle.js.map
